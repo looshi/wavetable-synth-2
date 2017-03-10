77d11bcb66147e50e43d92f9451ade1263f38fbf
looshi <davluzius@gmail.com>
2017-03-10 15:38:57 -0800

    Update about page w image.

--- src/views/Modals/AboutModal.js
+++ src/views/Modals/AboutModal.js
@@ -3,6 +3,7 @@ AboutModal
 */
 import React from 'react'
 import ReactModal from 'react-modal'
+import interpolatedWaves from '../../../images/interpolated-waves.png'
 
 export default class AboutModal extends React.Component {
 
@@ -23,7 +24,8 @@ export default class AboutModal extends React.Component {
             </p>
             <h2 className='text-center'>Wavetable Synthesis</h2>
             <p>A wavetable, in the simplest use of the term, is a list of samples for one period of a wave.  In practice, we store an array of 600 numbers for each waveform.  The more complicated use of the term "wavetable" synthesis refers to interpolating from one table to another over time.  Interpolating between these tables made it possible for digital synthesizers of the 1980s to generate complex and evolving sounds using very little processing power and memory.</p>
-            <p>All the interpolated values are precomputed into a potentially fairly long waveform ( what you see beneath each pair of waveform samples ). The "cycles" slider controls the amount of time it takes to change from one waveform to another.  The plus, minus, divide, and multiply buttons control the slope of interpolation.  Since we're not interpolating in real-time the loop time of each oscillator will change depending on pitch.  We would like to find a way to do this without it being dependent on pitch, if you happen to know please contact us!
+            <img src={interpolatedWaves} />
+            <p>We precompute all the interpolated values from one table to another into a new, and potentially much longer, waveform. The "cycles" slider controls the length of the interpolated waveform, this is how long it will take for the oscillator to smoothly change from one waveform to another.  The plus, minus, divide, and multiply buttons control the slope of interpolation.  Since we're not interpolating in real-time the loop time of each oscillator will change depending on pitch.  We would like to find a way to do this without it being dependent on pitch, if you happen to know please contact us!
             </p>
             <h2 className='text-center'>By No Means Perfect</h2>
             <p>To add texture in a few places we add a bit of randomness.  On each note, the pitch is altered randomly between 0 and 6 cents per oscillator.  Also, our square LFO uses a custom square waveform sample versus the preset square available in the web audio api, which does some very subtle ramping up and down and has some wobble to it.  Our filter is actually using 10 filters in sequence, we figured it's a computer, so why not use 10 filters?</p>
