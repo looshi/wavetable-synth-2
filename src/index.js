39e9eea71060858a1d7adef565646e9a71182af3
looshi <>
2022-12-10 14:46:32 -0500

    Update to use createRoot

--- src/index.js
+++ src/index.js
@@ -1,47 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
-import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
-
+import { createRoot } from 'react-dom/client';
+import App from './App.js'
+import Reducers from './data/Reducers.js'
 import '../css/index.scss'
 import '../css/keyboard.scss'
 import '../css/fader.scss'
 import '../css/react-select-overrides.scss'
-import App from './App.js'
-import Reducers from './data/Reducers.js'
+
 
 let store = createStore(Reducers)
-let audioContext
-const createAudioContext = () => {
-  audioContext = new AudioContext()
-  renderApp();
-}
 
-const renderApp = () => {
+const AppContainer = () => {
+  const [audioContext, setAudioContext] = useState(null);
+
   if (!audioContext) {
-    return render(
+    return (
       <div className="get-started">
         <h1>Welcome to Wavetable synth!</h1>
         <p>Click on the piano keys, type keys on your keyboard, or even hook up a MIDI keyboard to hear it in action!</p>
         <button
-          onClick={createAudioContext}>
+          onClick={() => setAudioContext(new AudioContext())}>
           Click Here to Start
         </button>
-      </div>,
-      document.getElementById('root')
+      </div>
     );
   }
 
-  return render(
+  return (
     <Provider store={store}>
       <App
         store={store}
         audioContext={audioContext}
       />
-    </Provider>,
-    document.getElementById('root')
+    </Provider>
   )
 }
 
-renderApp();
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<AppContainer />)
